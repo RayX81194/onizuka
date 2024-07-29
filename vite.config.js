eb491ef1c9f07e6872f2f5bb9f9969e788dadf13
rayx81194 <mani.phartiyal@gmail.com>
2024-07-29 13:09:52 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/onizuka/",
   server: {
     host: '0.0.0.0'  // or 'localhost' or your desired host
   }
